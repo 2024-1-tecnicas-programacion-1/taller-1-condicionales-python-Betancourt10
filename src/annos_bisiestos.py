047470e5dde40d8cde93b163ae1de0f51c18a5a1
Betancourt10 <Felibetan2005@gmail.com>
2024-03-11 06:40:13 -0500

    AnnosBisiestos.

--- src/annos_bisiestos.py
+++ src/annos_bisiestos.py
@@ -1,6 +1,12 @@
 def evaluar(anno):
-    # TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
-    return "";
+    if anno % 4 != 0: 
+        return str(anno) + "No es bisiesto" 
+    elif anno % 100 != 0 
+        return str(anno) + "Es bisiesto" 
+    elif anno % 400 == 0 
+        return str(anno) + "Es bisiesto" 
+    else: 
+        return str(anno) + "No es bisiesto";
 
 if __name__ == '__main__':
     print("Año:", end="")
