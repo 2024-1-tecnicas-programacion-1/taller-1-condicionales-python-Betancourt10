e81e2ced355b76942c5eb8f77ba9cd6750d94c21
Betancourt10 <Felibetan2005@gmail.com>
2024-03-12 01:08:07 -0500

    Test AnnosBisiestos.

--- tests/test.annos_bisiestos.py
+++ tests/test.annos_bisiestos.py
@@ -13,11 +13,34 @@ from src.annos_bisiestos import evaluar
 
 class TestAnnosBisiestos(unittest.TestCase):
     def test_1988(self):
-        valor_esperado = "1988 es bisiesto"
+        valor_esperado = "1988 Es bisiesto."
         valor_actual = evaluar(1988)
         self.assertEqual(valor_esperado, valor_actual)
     
-    # TODO: Agrega tus otros casos de prueba aquí
+    def test_2011(self):
+        valor_esperado = "2011 No es bisiesto."
+        valor_actual = evaluar(2011)
+        self.assertEqual(valor_esperado, valor_actual) 
+
+    def test_1700(self):
+        valor_esperado = "1700 No es bisiesto."
+        valor_actual = evaluar(1700)
+        self.assertEqual(valor_esperado, valor_actual) 
+
+    def test_2400(self):
+        valor_esperado = "2400 Es bisiesto."
+        valor_actual = evaluar(2400)
+        self.assertEqual(valor_esperado, valor_actual) 
+
+    def test_1500(self):
+        valor_esperado = "1500 No es bisiesto."
+        valor_actual = evaluar(1500)
+        self.assertEqual(valor_esperado, valor_actual) 
+
+    def test_2024(self):
+        valor_esperado = "2024 Es bisiesto."
+        valor_actual = evaluar(2024)
+        self.assertEqual(valor_esperado, valor_actual)
     
 
 if __name__ == '__main__':
