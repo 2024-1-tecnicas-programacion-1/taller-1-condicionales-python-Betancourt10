aea9bcc4d19a9829e10dc070e9389b154b89b9c3
Betancourt10 <Felibetan2005@gmail.com>
2024-03-12 02:45:48 -0500

    Set_de_tenis.

--- src/set_de_tenis.py
+++ src/set_de_tenis.py
@@ -1,6 +1,20 @@
 def evaluar(num_victorias_a, num_victorias_b):
-    # TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-    return ""
+    if num_victorias_a < 0 or num_victorias_b < 0: 
+        return "Error." 
+    elif num_victorias_a > 7 or num_victorias_b > 7: 
+        return "Inválido." 
+    elif num_victorias_a >= 6 and (num_victorias_a - num_victorias_b) >= 2: 
+        return "Ganó A." 
+    elif num_victorias_b >= 6 and (num_victorias_b - num_victorias_a) >= 2: 
+        return "Ganó B." 
+    elif num_victorias_a <= 6 and num_victorias_b <= 6: 
+        return "Aún no termina." 
+    elif num_victorias_a == 7 and num_victorias_b == 6: 
+        return "Ganó A." 
+    elif num_victorias_a == 6 and num_victorias_b == 7: 
+        return "Ganó B." 
+    else: 
+        return "Inválido." 
 
 if __name__ == '__main__':
     print("Los juegos ganaddor por A:", end="")
