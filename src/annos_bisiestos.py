11da8ece1f1dd9dde0260c445046b922f7249603
Betancourt10 <Felibetan2005@gmail.com>
2024-03-12 01:07:46 -0500

    Corrección AnnosBisiestos.

--- src/annos_bisiestos.py
+++ src/annos_bisiestos.py
@@ -1,12 +1,12 @@
 def evaluar(anno):
     if anno % 4 != 0: 
-        return str(anno) + "No es bisiesto" 
-    elif anno % 100 != 0 
-        return str(anno) + "Es bisiesto" 
-    elif anno % 400 == 0 
-        return str(anno) + "Es bisiesto" 
+        return str(anno) + " No es bisiesto." 
+    elif anno % 100 != 0: 
+        return str(anno) + " Es bisiesto." 
+    elif anno % 400 == 0: 
+        return str(anno) + " Es bisiesto." 
     else: 
-        return str(anno) + "No es bisiesto";
+        return str(anno) + " No es bisiesto."
 
 if __name__ == '__main__':
     print("Año:", end="")
