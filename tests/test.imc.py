fe2397835cb1243dcc3d7a466274b4cc6fc40b77
Betancourt10 <Felibetan2005@gmail.com>
2024-03-12 01:53:18 -0500

    Test IMC.

--- tests/test.imc.py
+++ tests/test.imc.py
@@ -13,11 +13,29 @@ from src.imc import evaluar
 
 class TestIMC(unittest.TestCase):
     def testBajo(self):
-        valor_esperado = "bajo"
+        valor_esperado = "Bajo."
         valor_actual = evaluar(50, 1.8,   20)
         self.assertEqual(valor_esperado, valor_actual)
     
-    # TODO: Agrega tus otros casos de prueba aquí
+    def testMedio(self):
+        valor_esperado = "Medio."
+        valor_actual = evaluar(76, 1.69,   34)
+        self.assertEqual(valor_esperado, valor_actual) 
+
+    def testMedioDos(self):
+        valor_esperado = "Medio."
+        valor_actual = evaluar(44, 1.7,   52)
+        self.assertEqual(valor_esperado, valor_actual) 
+
+    def testAlto(self):
+        valor_esperado = "Alto."
+        valor_actual = evaluar(70, 1.65,   73)
+        self.assertEqual(valor_esperado, valor_actual) 
+
+    def testError(self):
+        valor_esperado = "Error."
+        valor_actual = evaluar(-2, 1.8,   18)
+        self.assertEqual(valor_esperado, valor_actual)
     
 
 if __name__ == '__main__':
