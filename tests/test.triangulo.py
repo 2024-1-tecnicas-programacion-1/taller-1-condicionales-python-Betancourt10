b6313c8e4aa02e1c38f819f74835a26bb4f2e454
Betancourt10 <Felibetan2005@gmail.com>
2024-03-12 02:00:58 -0500

    Test Triángulo.

--- tests/test.triangulo.py
+++ tests/test.triangulo.py
@@ -13,11 +13,24 @@ from src.triangulo import evaluar
 
 class TestTriangulo(unittest.TestCase):
     def test_no_es_un_triangulo_valido(self):
-        valor_esperado = "No es un triángulo válido"
+        valor_esperado = "No es un triángulo válido."
         valor_actual = evaluar(3.9, 6.0, 1.2)
         self.assertEqual(valor_esperado, valor_actual)
     
-    # TODO: Agrega tus otros casos de prueba aquí
+    def test_es_un_triangulo_equilatero(self):
+        valor_esperado = "Es un triángulo equilátero."
+        valor_actual = evaluar(3, 3, 3)
+        self.assertEqual(valor_esperado, valor_actual) 
+
+    def test_es_un_triangulo_isosceles(self):
+        valor_esperado = "Es un triángulo isósceles."
+        valor_actual = evaluar(5, 8, 5)
+        self.assertEqual(valor_esperado, valor_actual) 
+
+    def test_es_un_triangulo_escaleno(self):
+        valor_esperado = "Es un triángulo escaleno."
+        valor_actual = evaluar(8, 2, 7)
+        self.assertEqual(valor_esperado, valor_actual)
     
 
 if __name__ == '__main__':
