5bd3b228015347eea802e4d392f0a3c2ed53d1cd
Betancourt10 <Felibetan2005@gmail.com>
2024-03-12 01:26:11 -0500

    Test División.

--- tests/test.division.py
+++ tests/test.division.py
@@ -12,14 +12,38 @@ sys.path.append(str(root_path))
 from src.division import evaluar
 
 class TestDivision(unittest.TestCase):
-    def testDivisionExacta(self):
-        valor_esperado = "La división es exacta. \n" \
+    def testDivisionNoExacta(self):
+        valor_esperado = "La división no es exacta. \n" \
                          "Cociente: 2\n" \
                          "Residuo: 4"
         valor_actual = evaluar(14, 5)
         self.assertEqual(valor_esperado, valor_actual)
     
-    # TODO: Agrega tus otros casos de prueba aquí
+    def testDivisionExacta(self):
+        valor_esperado = "La división es exacta. \n" \
+                         "Cociente: 10\n" \
+                         "Residuo: 0"
+        valor_actual = evaluar(100, 10)
+        self.assertEqual(valor_esperado, valor_actual) 
+
+    def testDivisionNoExactaDos(self):
+        valor_esperado = "La división no es exacta. \n" \
+                         "Cociente: 6\n" \
+                         "Residuo: 2"
+        valor_actual = evaluar(32, 5)
+        self.assertEqual(valor_esperado, valor_actual) 
+
+    def testDivisionExactaDos(self):
+        valor_esperado = "La división es exacta. \n" \
+                         "Cociente: 7\n" \
+                         "Residuo: 0"
+        valor_actual = evaluar(56, 8)
+        self.assertEqual(valor_esperado, valor_actual) 
+
+    def testDivisionNoValida(self):
+        valor_esperado = "La división no es válida."
+        valor_actual = evaluar(2, 0)
+        self.assertEqual(valor_esperado, valor_actual)
     
 
 if __name__ == '__main__':
